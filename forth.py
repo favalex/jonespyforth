79295fbea71a6c99a4b82b683d486bb2d66f6f6c
Gabriele Favalessa <gabriele@develer.com>
2010-10-29 22:34:05 +0200

    lots of new words

--- forth.py
+++ forth.py
@@ -122,6 +122,18 @@ def defvar(name, value):
         vars[name] = value
         define(name, 0, Var(name))
 
+def tdfa(frame):
+    stack.push(Ref(stack.pop()))
+define('>DFA', 0, tdfa)
+
+def tffa(frame):
+    stack.push(stack.pop().flags)
+define('>FFA', 0, tffa)
+
+def execute_(frame):
+    execute(Frame([stack.pop()]))
+define('EXECUTE', 0, execute_)
+
 defvar('STATE', 0)
 defvar('BASE', 10)
 defvar('HERE', [None]*100)
@@ -138,8 +150,55 @@ def dspstore(frame):
 
 define('DSP!', 0, dspstore)
 
+def rspfetch(frame):
+    stack.push(len(return_stack)-1)
+define('RSP@', 0, rspfetch)
+
+def rspstore(frame):
+    return_stack.set_top(stack.pop())
+define('RSP!', 0, rspstore)
+
 def dup(frame):
     stack.push(stack.peek())
+
+def qdup(frame):
+    if stack.peek() != 0:
+        stack.push(stack.pop())
+define('?DUP', 0, qdup)
+
+def over(frame):
+    stack.push(stack[-2])
+define('OVER', 0, over)
+
+def rot(frame):
+    # a b c -> b c a
+    c = stack.pop()
+    b = stack.pop()
+    a = stack.pop()
+    stack.push(b)
+    stack.push(c)
+    stack.push(a)
+define('ROT', 0, rot)
+
+def nrot(frame):
+    # a b c -> c a b
+    c = stack.pop()
+    b = stack.pop()
+    a = stack.pop()
+    stack.push(c)
+    stack.push(a)
+    stack.push(b)
+define('-ROT', 0, nrot)
+
+def twodrop(frame):
+    stack.pop()
+    stack.pop()
+define('2DROP', 0, twodrop)
+
+def twodup(frame):
+    stack.push(stack[-2])
+    stack.push(stack[-2])
+define('2DUP', 0, twodup)
 
 def swap(frame):
     a = stack.pop()
@@ -154,11 +213,23 @@ define('DROP', 0, drop)
 def fetch(frame):
     stack.push(stack.pop().fetch())
 define('@', 0, fetch)
+define('C@', 0, fetch)
 
 def store(frame):
     var = stack.pop()
     var.store(stack.pop())
 define('!', 0, store)
+define('C!', 0, store)
+
+def addstore(frame):
+    var = stack.pop()
+    var.store(var.fetch() + stack.pop())
+define('+!', 0, addstore)
+
+def substore(frame):
+    var = stack.pop()
+    var.store(var.fetch() - stack.pop())
+define('-!', 0, substore)
 
 def comma(frame):
     here = vars['HERE']
@@ -197,6 +268,18 @@ def divmod_(frame):
     stack.push(mod)
     stack.push(div)
 
+def tor(frame):
+    return_stack.push(stack.pop())
+define('>R', 0, tor)
+
+def fromr(frame):
+    stack.push(return_stack.pop())
+define('R>', 0, fromr)
+
+def rdrop(frame):
+    return_stack.pop()
+define('RDROP', 0, rdrop)
+
 def binary(operator):
     def word(frame):
         x = stack.pop()
@@ -211,6 +294,13 @@ def boolean(operator):
         stack.push(1 if operator(stack.pop(), x) else 0)
 
     word.__name__ = operator.__name__
+    return word
+
+def zboolean(operator):
+    def word(frame):
+        stack.push(1 if operator(stack.pop(), 0) else 0)
+
+    word.__name__ = 'z' + operator.__name__
     return word
 
 def rspstore(frame):
@@ -358,12 +448,21 @@ define('/MOD', 0, divmod_)
 define('*', 0, binary(operator.mul))
 define('+', 0, binary(operator.add))
 define('-', 0, binary(operator.sub))
+define('AND', 0, binary(operator.iand))
+define('OR', 0, binary(operator.ior))
+define('XOR', 0, binary(operator.ixor))
 define('=', 0, boolean(operator.eq))
 define('<>', 0, boolean(operator.ne))
 define('<', 0, boolean(operator.lt))
 define('>', 0, boolean(operator.gt))
 define('>=', 0, boolean(operator.ge))
 define('<=', 0, boolean(operator.le))
+define('0=', 0, zboolean(operator.eq))
+define('0<>', 0, zboolean(operator.ne))
+define('0<', 0, zboolean(operator.lt))
+define('0>', 0, zboolean(operator.gt))
+define('0>=', 0, zboolean(operator.ge))
+define('0<=', 0, zboolean(operator.le))
 define('PRINT', 0, print_)
 define('INTERPRET', 0, interpret)
 define('QUIT', 0, ['R0', 'RSP!', 'INTERPRET', 'BRANCH', -4])
@@ -374,6 +473,16 @@ define('WORD', 0, word)
 def incr(frame):
     stack.push(1 + stack.pop())
 define('1+', 0, incr)
+define('4+', 0, incr)
+
+def decr(frame):
+    stack.push(stack.pop() - 1)
+define('1-', 0, decr)
+define('4-', 0, decr)
+
+def invert(frame):
+    stack.push(~stack.pop())
+define('INVERT', 0, invert)
 
 def nop(frame):
     pass
@@ -381,11 +490,25 @@ def nop(frame):
 define(':', 0, ['WORD', 'CREATE', ']'])
 define(';', IMMED, ['FINISH', '['])
 def tick(frame):
-    stack.push(frame.get_current_instruction())
-    frame.next()
+    stack.push(frame.next())
 define('\'', 0, tick)
 define('\'\'', 0, ['WORD', 'FIND', 'DROP'])
 define('>CFA', 0, nop)
+
+define('DOCOL', 0, nop)
+
+def litstring(frame):
+    n = frame.next()
+    s = frame.next()
+    stack.push(s)
+    stack.push(n)
+define('LITSTRING', 0, litstring)
+
+def tell(frame):
+    n = stack.pop()
+    s = stack.pop()
+    sys.stdout.write(s)
+define('TELL', 0, tell)
 
 def getenv(frame):
     import os
@@ -441,6 +564,10 @@ def execute(frame):
 
 define('DOUBLE', 0, ['DUP', '+'])
 define('DOUBLE2', 0, ['LIT', 2, '*'])
+
+def exit(frame):
+    pass
+define('EXIT', 0, exit)
 
 # execute(Frame(compile(['LIT', 10, 'DOUBLE', 'PRINT'])))
 args = sys.argv[1:]
